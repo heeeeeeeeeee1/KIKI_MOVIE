92bac6dc1804e0731dc8b3d7bf0c43af4620c350
minjeeki <minjeeki@student.42seoul.kr>
2024-11-21 06:39:57 +0900

    Modify: script setup and style
    - 교안 내용 srcipt 옮김
    - css 현재 설정된 것 하단에 style 설정 (덮어씌움)

--- front/vue-project/src/App.vue
+++ front/vue-project/src/App.vue
@@ -1,16 +1,32 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+
+const logOut = function () {
+  store.logOut()
+}
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink :to="{ name: 'SignUpView' }">회원가입</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <nav class="left-menu">
+      <span>로고</span>
+      <a href="">홈</a>
+      <a href="">영화</a>
+      <!-- <RouterLink :to="{ name: 'home' }">홈</RouterLink>
+      <RouterLink :to="{ name: 'movie' }">영화</RouterLink> -->
 
+    </nav>
+    <nav class="right-users">
+      <RouterLink :to="{ name: 'SignUpView' }" class="users__signup">회원가입</RouterLink>
+      <RouterLink :to="{ name: 'LogInView' }" class="users__login">로그인</RouterLink>
+      <form @submit.prevent="logOut">
+        <input type="submit" value="Logout" class="users__logout">
+      </form>
+    </nav>
+  </header>
   <RouterView />
 </template>
 
@@ -29,7 +45,6 @@ nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
@@ -44,6 +59,7 @@ nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
+  text-wrap: nowrap;
 }
 
 nav a:first-of-type {
@@ -75,5 +91,45 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
+}
+
+/* 새롭게 작성한 내용 */
+header {
+  width: 100%;
+  min-height: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+  padding: 0.5rem 0;
+  /* position: fixed; */
+}
+
+nav {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 0 1rem;
+}
+nav.right-users {
+  justify-content: flex-end;
+}
+nav > a, span, .logo, .users__logout, .users__signup, .users__login {
+  text-decoration: none;
+  color: white;
+}
+nav a.router-link-exact-active {
+  color: white;
+}
+.right-users form {
+  height: 100%;
+}
+.users__logout {
+  background-color: transparent;
+  border: none;
+  height: 100%;
+  padding: 0 1rem;
+  line-height: 1.5rem;
 }
 </style>
