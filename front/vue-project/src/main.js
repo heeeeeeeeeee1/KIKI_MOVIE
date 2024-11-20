7e1c94b7c1bdfb80e61862b0c7ed8ce9cc10be52
minjeeki <minjeeki@student.42seoul.kr>
2024-11-21 06:41:32 +0900

    Modify: routers, stores, main.js with lecture code
    - 수업 교안에 맞춰 일단 바꿔둠

--- front/vue-project/src/main.js
+++ front/vue-project/src/main.js
@@ -1,16 +1,24 @@
-// main.js
+// main.js : Vue.js의 entry point(진입점) 역할을 하는 파일. 애플리케이션의 전반적인 설정과 초기화를 담당함.
+
+// Vue.js 애플리케이션을 생성하고 설정하는 데 사용되는 함수들을 가져옴
+// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'     // Pinia 상태 영속화 플러그인
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import axios from 'axios'                                               // HTTP 요청 라이브러리 (npm install로 추가함)
+// 로컬에서 작성한 파일들(앱 컴포넌트 및 라우터)을 import함
 import App from './App.vue'
 import router from './router'
 
+// Vue 애플리케이션 생성 및 플러그인 등록
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+// pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
-app.mount('#app')
+// axios 글로벌 속성 추가 : Axios를 Vue 컴포넌트 내에서 this.$axios로 접근할 수 있도록 하기 위함
+app.config.globalProperties.$axios = axios
 
-// 추가함. Pinia와 Axios를 글로벌 속성으로 설정?
-app.config.globalProperties.$axios = axios 
+// 앱 마운트 : Vue 애플리케이션을 HTML의 특정 DOM 요소(#app)에 연결하는 역할. 이후 Vue 컴포넌트들이 해당 DOM 요소 내에서 렌더링되고 상호작용할 수 있음.
+app.mount('#app')
--- front/vue-project/src/stores/counter.js
+++ front/vue-project/src/stores/counter.js
@@ -1,17 +1,95 @@
 // src/stores/counter.js
+import { ref, computed } from 'vue';
 import { defineStore } from "pinia";
+import axios from 'axios'
+import { useRouter } from 'vue-router'
 
-export const useCounterStore = defineStore("counter", {
-  state: () => ({
-    count: 0,
-  }),
-  actions: {
-    increment() {
-      this.count++;
-    },
-    signUp(payload) {
-      console.log("Sign up payload from store:", payload);
-      // 회원가입 로직 추가
-    },
-  },
-});
+
+// export const useCounterStore = defineStore("counter", {
+//   state: () => ({
+//     count: 0,
+//   }),
+//   actions: {
+//     increment() {
+//       this.count++;
+//     },
+//     signUp(payload) {
+//       console.log("Sign up payload from store:", payload);
+//       // 회원가입 로직 추가
+//     },
+//   },
+// });
+
+export const useCounterStore = defineStore('counter', () => {
+  const API_URL = 'http://127.0.0.1:8000'
+  const token = ref(null)
+  const isLogin = computed(() => {
+    if (token.value === null) {
+      return false
+    } else {
+      return true
+    }
+  })
+  const router = useRouter()
+
+  // 회원가입 요청 액션
+  const signUp = function (payload) {
+    const { username, password1, password2 } = payload
+
+    axios({
+      method: 'post',
+      url: `${API_URL}/accounts/signup/`,
+      data: {
+        username, password1, password2
+      }
+    })
+      .then((res) => {
+        // console.log(res)
+        // console.log('회원가입 성공')
+        const password = password1
+        logIn({ username, password })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  // 로그인 요청 액션
+  const logIn = function (payload) {
+    const { username, password } = payload
+
+    axios({
+      method: 'post',
+      url: `${API_URL}/accounts/login/`,
+      data: {
+        username, password
+      }
+    })
+      .then((res) => {
+        token.value = res.data.key
+        router.push({ name: 'ArticleView' })
+        // console.log(res.data)
+        // console.log('로그인 성공')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  
+  // [추가기능] 로그아웃
+  const logOut = function () {
+    axios({
+      method: 'post',
+      url: `${API_URL}/accounts/logout/`,
+    })
+      .then((res) => {
+        console.log(res.data)
+        token.value = null
+        router.push({ name: 'ArticleView' })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  return { API_URL, signUp, logIn, token, isLogin, logOut }
+}, { persist: true })
