7860a9c52efed10b15480e436f14b734a07af812
minjeeki <minjeeki@student.42seoul.kr>
2024-11-20 15:44:09 +0900

    Add: admin, signals.py in accounts

--- back/accounts/admin.py
+++ back/accounts/admin.py
@@ -5,21 +5,35 @@ from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     # 사용자 정보 수정 화면에 추가 필드 표시
-    fieldsets = UserAdmin.fieldsets + (
-        ('추가 정보', {  # 새 섹션 이름
-            'fields': ('gender', 'birth_date', ),  # 추가할 필드
+    fieldsets = (
+        ("Infos", {
+            "fields": ("email", "password", "username", "gender", "birth_date"),  # 추가할 필드
+        }),
+        ("Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            ),
+            "classes": ("collapse", ),
+        }),
+        ("Important dates", {
+            "fields": ("last_login", "date_joined"),
+            "classes": ("collapse", ),
+        })
+    )
+    # 유저 추가 시 나타나는 폼 커스터마이징
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2", "gender", "birth_date"),  # 추가 필드
         }),
     )
 
-    # 사용자 생성 화면에 추가 필드 표시
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('추가 정보', {
-            'classes': ('wide',),
-            'fields': ('gender', 'birth_date', 'email', ),
-        }),
-    )
-
-    # 리스트 화면에서 표시할 필드
+    # 리스트 화면에서 표시할 필드 및 정렬 설정
     list_display = ('username', 'email', 'gender', 'birth_date', 'is_staff')
     list_filter = ('gender', 'is_staff', 'is_superuser')
-	
+    search_fields = ('username', 'email')
+    ordering = ('username',)
--- back/accounts/signals.py
+++ back/accounts/signals.py
@@ -4,6 +4,6 @@ from rest_framework.authtoken.models import Token
 from django.conf import settings
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_user_profile(sender, instance=None, created=False, **kwargs):
+def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
