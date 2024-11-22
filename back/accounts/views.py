cfe5867839ed2bd95689f1b3fea394cfddf3e73c
Zoe <ddu_buk_e@naver.com>
2024-11-22 17:57:45 +0900

    Modify: accounts url, wiews 임시 작성

--- back/accounts/views.py
+++ back/accounts/views.py
@@ -1,3 +1,16 @@
+# accounts/urls.py
 from django.shortcuts import render
 
 # Create your views here.
+from .serializers import *
+from movies.serializers import *
+
+User = get_user_model()
+
+# 개인 프로필 조회
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_info(request, username):
+    user = get_object_or_404(User, username=username)
+    serializer = UserInfoSerializers(user)
+    return Response(serializer.data)
