72f6be61b7eb83b95d9025a306d156fddebb5243
minjeeki <minjeeki@student.42seoul.kr>
2024-11-24 05:39:13 +0900

    Fix: Add import which hidden
    - 주석처리된 import ISAuthenticated 수정

--- back/movies/views.py
+++ back/movies/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from django.shortcuts import get_object_or_404, get_list_or_404
-# from rest_framework.decorators import permission_classes
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .models import *
 from .serializers import *
