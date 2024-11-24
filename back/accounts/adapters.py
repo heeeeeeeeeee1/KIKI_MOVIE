d0b08359fa92ebb3beeb555a1d5555182f80c2be
minjeeki <minjeeki@student.42seoul.kr>
2024-11-24 10:30:11 +0900

    Fix: 회원정보에서 gender, birthdate 받을 수 있도록 수정

---
+++ back/accounts/adapters.py
@@ -0,0 +1,11 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.utils import user_field
+
+class CustomUserAccountAdapter(DefaultAccountAdapter):
+    def save_user(self, request, user, form, commit=True):
+        user = super().save_user(request, user, form, False)  # 기본 동작 수행
+        user_field(user, 'gender', request.data.get('gender'))
+        user_field(user, 'birth_date', request.data.get('birth_date'))
+        if commit:
+            user.save()
+        return user
