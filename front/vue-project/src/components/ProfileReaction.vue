50df59ed391d3f9682ace210227462cbd4391048
minjeeki <minjeeki@student.42seoul.kr>
2024-11-24 22:13:56 +0900

    Feat: load more data and connect links
    - 리뷰에 영화 데이터 없어서 연결
    - 각 리뷰, 영화 클릭 시 해당 상세 페이지로 연결

--- front/vue-project/src/components/ProfileReaction.vue
+++ front/vue-project/src/components/ProfileReaction.vue
@@ -18,13 +18,23 @@
 
   <!-- 봤어요 -->
   <div class="watched-info" v-if="activeList === 'watched'">
-    <!-- 봤어요 힝목 클릭 시 해당 리뷰 상세 페이지로 이동 -->
-    <div v-for="movie in watchedMovies" :key="movie.id" class="watched-items">
-      <!-- 영화 관련 정보 - title, poster 안나옴 -->
-      <h4>{{ movie.title }}</h4>
-      <p>평점: {{ movie.score }}</p>
-      <p>{{ movie.content }}</p>
-      <!-- 댓글과 코멘트 보이게 하기 -->
+    <div
+      v-for="review in watchedMovies"
+      :key="review.id"
+      class="watched-items"
+      @click="goToReviewDetail(review.movie.id, review.id)"
+    >
+      <img :src="tmdb_base_url + review.movie.poster_path" alt="movie-poster" />
+      <h4>{{ review.movie.title }}</h4>
+      <div class="genre-badges">
+        <span v-for="genre in review.movie_genres" :key="genre" class="genre-badge">
+          {{ genre }}
+        </span>
+      </div>
+      <p>평점: {{ review.score }}</p>
+      <p>리뷰: {{ review.content }}</p>
+      <p>댓글: {{ review.comment_count }}개</p>
+      <p>좋아요: {{ review.like_count }}개</p>
     </div>
   </div>
 
@@ -33,11 +43,20 @@
     <div v-if="activeList === 'wishlist'" class="wish-list">
       <!-- 보고싶어요 항목 클릭 시 해당 영화 상세 페이지로 이동 -->
       <!-- 영화 타이틀, 포스터 이미지만 가져올 것 -->
-      <div v-for="movie in wishlistMovies" :key="movie.id" class="wish-items">
+      <div 
+        v-for="movie in wishlistMovies" 
+        :key="movie.id" 
+        class="wish-items"
+        @click="goToMovieDetail(movie.id)"
+      >
         <img :src="tmdb_base_url + movie.poster_path" alt="movie-poster" class="wish-poster" />
         <div class="wish-details">
           <h4>{{ movie.title }}</h4>
-          <!-- 장르 관련 키워드 인자 리스트업하기 -->
+          <div class="genre-badges">
+          <span v-for="genre in movie.genres" :key="genre.id" class="genre-badge">
+            {{ genre.name }}
+          </span>
+        </div>
         </div>
       </div>
     </div>
@@ -45,12 +64,24 @@
 </template>
 
 <script setup>
+import { useRouter } from "vue-router";
+
 defineProps(['activeList', 'watchedMovies', 'wishlistMovies']);
 const emit = defineEmits(['switchList']);
-
+const router = useRouter();
 // 리스트 전환
 const switchTo = (listName) => {
   emit('switchList', listName);
+};
+
+// "봤어요" 상세 페이지로 이동
+const goToReviewDetail = (movieId, reviewId) => {
+  router.push({ name: "ReviewDetailView", params: { moviePk: movieId, reviewPk: reviewId } });
+};
+
+// "보고싶어요" 영화 상세 페이지로 이동
+const goToMovieDetail = (movieId) => {
+  router.push({ name: "MovieDetailView", params: { moviePk: movieId } });
 };
 
 const tmdb_base_url = "https://image.tmdb.org/t/p/w500"
