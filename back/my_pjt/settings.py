712b0291ec164d37409da0bee89feded9c446aa9
minjee kim <148981647+minjeeki@users.noreply.github.com>
2025-05-26 21:24:02 +0900

    Add CORS_ALLOWED_ORIGINS

--- back/my_pjt/settings.py
+++ back/my_pjt/settings.py
@@ -170,6 +170,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://kiki-movie.vercel.app',
     ]
 
 REST_FRAMEWORK = {
